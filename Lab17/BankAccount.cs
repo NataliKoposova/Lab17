838bd821ae3b69ed3958f6cc19218b8744a5698a
Koposova1 <Koposova1@KOPOSOVANA>
2024-04-16 12:47:46 +0300

    Добавьте файлы проекта.

---
+++ Lab17/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    class BankAccount <T>
+    {
+        T accountNumber;
+        double balance;
+        string ownersName;
+        public BankAccount(T accountNumber, double balance, string ownersName)
+        {
+            this.accountNumber = accountNumber;
+            this.balance = balance;
+            this.ownersName = ownersName;
+        }
+
+        public T GetAccountNumber()
+        { return accountNumber; }
+        public void SetAccountNumber(T accountNumber)
+        { this.accountNumber = accountNumber; }
+        public double GetBalance() { return balance; }
+        public void SetBalance(double balance)
+        { this.balance = balance; }
+        public string GetOwnersName() { return ownersName; }
+        public void SetOwnersName(string ownersName)
+        { this.ownersName = ownersName; }
+        public void ShowData()
+        {
+            Console.WriteLine($"Имя владельца: {ownersName}, баланс: {balance}, номер счета: {accountNumber}");
+        }
+    }
+}
---
+++ Lab17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long accountNumber1 = 2020200220202020;
+            double balance1 = 100000;
+            string owner1 = "Копосова Наталия Александровна";
+            string accountNumber2 = "SBER_00000001";
+            double balance2 = 200000;
+            string owner2 = "Копосова Елена Александровна";
+            BankAccount<long> intAccount = new BankAccount<long>(accountNumber1, balance1, owner1);
+            BankAccount<string> stringAccount = new BankAccount<string>(accountNumber2, balance2, owner2);
+            intAccount.ShowData();
+            stringAccount.ShowData();
+            Console.ReadKey();  
+        }
+        
+    }
+    
+}
